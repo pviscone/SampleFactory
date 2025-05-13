b81266fe65f945c9a3335b53fa576a04aef2418d
Piero Viscone <piero.viscone@cern.ch>
2025-05-13 17:30:51 +0200

    Added Double Electron gun FlatLxy

---
+++ data/fragments/DisplacedDoubleElectronGun.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDProducer("FlatRandomPtAndLxyGunProducer",
+    PGunParameters = cms.PSet(
+        PartID = cms.vint32(11),
+        MinPt  = cms.double(0.5),
+        MaxPt  = cms.double(20.00),
+        MinEta = cms.double(-2.5),
+        MaxEta = cms.double(2.5),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        LxyMin = cms.double(0.),
+        LxyMax = cms.double(2.0),
+        LzMax = cms.double(100.0),
+    ),
+    Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+    psethack = cms.string('displaced electron'),
+    AddAntiParticle = cms.bool(True),
+    firstRun = cms.untracked.uint32(1)
+)
