0dce9eefdd58c452e03c7085e3a05fdff7feb979
Piero Viscone <pieroviscone@gmail.com>
2026-01-28 14:48:53 +0100

    Fixed fastpuppi branch in chains

---
+++ data/fragments/DYToLL_M-10To50_TuneCP5_14TeV-pythia8.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+
+
+generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
+                         comEnergy = cms.double(14000.0),
+                         maxEventsToPrint = cms.untracked.int32(1),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         pythiaPylistVerbosity = cms.untracked.int32(1),
+                         PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'WeakSingleBoson:ffbar2gmZ = on',
+            '23:onMode = off',
+            '23:onIfAny = 11',
+            '23:mMin = 10.',
+            '23:mMax = 50.',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+        )
+                         )
+
+ProductionFilterSequence = cms.Sequence(generator)
