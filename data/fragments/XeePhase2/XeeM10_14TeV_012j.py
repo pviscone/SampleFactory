e635be17359cd66a7d227a05eb4bd858cc14b4d1
Piero Viscone <pieroviscone@gmail.com>
2026-01-28 12:32:46 +0100

    Use new gridpacks in Xee fragments

--- data/fragments/XeePhase2/XeeM10_14TeV_012j.py
+++ data/fragments/XeePhase2/XeeM10_14TeV_012j.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 
-gridpackPath="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/Scouting/gridpacks/Xee/Mass10_slc7_amd64_gcc900_CMSSW_12_0_2_tarball.tar.xz"
+gridpackPath="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/Scouting/gridpacks/Xee/v2/Mass10_el8_amd64_gcc12_CMSSW_15_0_2_tarball.tar.xz"
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     args = cms.vstring(gridpackPath),
