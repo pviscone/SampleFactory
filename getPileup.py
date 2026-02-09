85333dde4f32c27bcc55f5c91bc2c8ed7faa09e8
Piero Viscone <piero.viscone@cern.ch>
2026-02-09 14:39:24 +0100

    Create pileups dir if not exist

--- getPileup.py
+++ getPileup.py
@@ -52,6 +52,7 @@ class GetPileup:
         pileup_dataset = pileup_dataset[0].replace("dbs:", "").replace("\"","")
         pileup_txt = f"{self.CHAIN_NAME}.txt"
         sites = ["T1_DE_KIT_Disk", "T1_ES_PIC_Disk", "T1_FR_CCIN2P3_Disk", "T1_IT_CNAF_Disk", "T1_UK_RAL_Disk", "T1_US_FNAL_Disk", "T2_AT_Vienna", "T2_BE_IIHE", "T2_BE_UCL", "T2_CH_CERN", "T2_CH_CSCS", "T2_DE_DESY", "T2_DE_RWTH", "T2_ES_CIEMAT", "T2_ES_IFCA", "T2_FR_GRIF", "T2_FR_IPHC", "T2_IT_Bari", "T2_IT_Legnaro", "T2_IT_Pisa", "T2_IT_Rome", "T2_UK_London_Brunel", "T2_UK_London_IC", "T2_UK_SGrid_RALPP", "T2_US_Caltech", "T2_US_Florida", "T2_US_MIT", "T2_US_Nebraska", "T2_US_Purdue", "T2_US_UCSD", "T2_US_Vanderbilt", "T2_US_Wisconsin"]
+        os.makedirs("data/pileups", exist_ok=True)
         if os.path.exists(f"data/pileups/{pileup_txt}"):
             os.system(f"rm data/pileups/{pileup_txt}")
         os.system(f"touch data/pileups/{pileup_txt}")
